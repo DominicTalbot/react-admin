fadbf87f76f2e1dcbf918af6b20737079acf6a26
Dominic Talbot <Dominictalbot@live.co.uk>
2025-01-17 15:50:01 -0500

    Implement theme context and layout structure with Topbar and Sidebar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-function App() {
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
 
-  return <div className= "app"> </div>;
+function App() {
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode} >
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider >
+  );
 }
+
 export default App;
